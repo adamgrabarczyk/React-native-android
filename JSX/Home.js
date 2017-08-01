0ed851d96cd9defe1718eb29022ce8645c7846c1
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-01 08:56:45 +0200

    add home component

---
+++ JSX/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import { StyleSheet } from 'react-native'
+
+export default class LoginForm extends Component  {
+    render() {
+        return (
+
+            console.log(5)
+        );
+
+    }
+}
+
+
+const styles = StyleSheet.create({
+
+
+})
